a6bc09260c28a9278bb69cc98c275009ce2fac37
Jeremieon <jerry_slade@outlook.com>
2024-02-24 00:35:27 +0100

    preparing to add DB

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,3 +1,10 @@
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
 class Settings:
     PROJECT_NAME:str = "Jeremiah Blog🔥"
     PROJECT_VERSION: str = "0.0.1"
