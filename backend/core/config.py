4f614cfa0b719affd74ca80e73edaed5b7ab6931
Jeremieon <jerry_slade@outlook.com>
2024-02-24 02:28:49 +0100

    added database

--- backend/core/config.py
+++ backend/core/config.py
@@ -2,9 +2,7 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path
 
-env_path = Path('.') / '.env'
-load_dotenv(dotenv_path=env_path)
-
+load_dotenv()
 class Settings:
     PROJECT_NAME:str = "Jeremiah Blog🔥"
     PROJECT_VERSION: str = "0.0.1"
---
+++ backend/db/base.py
@@ -0,0 +1,5 @@
+from db.base_class import Base
+from db.models.user import User
+from db.models.blog import Blog
+#alembic revision --autogenerate -m "first commit"
+#alembic upgrade head
