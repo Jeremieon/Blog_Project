528dbb9fe88b9d37326e187f60f439c69241af3e
Jeremieon <jerry_slade@outlook.com>
2024-02-24 03:05:47 +0100

    added user

--- backend/db/session.py
+++ backend/db/session.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from core.config import settings
-
+from typing import Generator  
 
 SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
 print("Database URL is ",SQLALCHEMY_DATABASE_URL)
@@ -9,3 +9,10 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 
 SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
+
+def get_db() -> Generator:   #new
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
